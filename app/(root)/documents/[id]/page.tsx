0f8f45391fb009fefb7a1ced25388b70a3859cb3
Mehrzad Ardeshiri <mehrzad.ardeshiri@gmail.com>
2025-01-12 11:35:13 +0330

    initial components

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Editor } from "@/components/Editor";
+import React from "react";
+
+const Document = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
+
+export default Document;
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+
+declare type HeaderProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+

a1514a41a7776ea1da86af7201d685b2b76ea015
Mehrzad Ardeshiri <mehrzad.ardeshiri@gmail.com>
2025-01-18 16:56:03 +0330

    room creation and providers

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { Editor } from "@/components/Editor";
+
 import React from "react";
 
 const Document = () => {
   return (
     <div>
-      <Editor />
+  
     </div>
   );
 };
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,20 @@
 /* eslint-disable no-unused-vars */
 
+declare type AccessType = ["room:write"] | ["room:read", "room:presence:write"];
+
+declare type RoomAccesses = Record<string, AccessType>;
+
 declare type HeaderProps = {
   children: React.ReactNode;
   className?: string;
 };
 
+declare type CreateDocumentParams = {
+  userId: string;
+  email: string;
+};
+
+declare type AddDocumentBtnProps = {
+  userId: string;
+  email: string;
+};
